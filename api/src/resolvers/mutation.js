9107ac1bef6f03cf0d760c212f0694af780d9a48
johnmiicheal <michealelijah301@gmail.com>
2022-04-22 21:09:35 -0700

    user auth mutation

--- api/src/resolvers/mutation.js
+++ api/src/resolvers/mutation.js
@@ -1,3 +1,15 @@
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const {
+    AuthenticationError, 
+    ForbiddenError
+} = require('apollo-server-express');
+
+require('dotenv').config();
+const gravatar = require('../util/gravatar');
+
+
 module.exports = {
     newNote: async (parent, args, { models }) => {
         return await models.Note.create({
@@ -28,7 +40,24 @@ module.exports = {
             }
         );
     },
+    signUp: async (parent, { username, email, password }, { models }) => {
+        email = email.trim().toLowerCase();
+        const hashed = await bcrypt.hash(password, 10);
+        const avatar = gravatar(email);
+        try{
+            const user = await models.User.create({
+                username, 
+                email,
+                avatar,
+                password: hashed
+            });
+
+            return jwt.sign({ id: user._id }, process.env.JWT_SECRET)
+        } catch(err) {
+            console.log(err);
+            throw new Error('Error creating account');
+        }
+    }
     
 }
 
-const bcrypt = require('bcrypt')
